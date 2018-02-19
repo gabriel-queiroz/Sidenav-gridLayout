ffbf497fe8561ee3ce78e8ca3d7ab4dd09cb5767
Gabriel Queiroz do Nascimento <gabrielqueirozzn@gmail.com>
2018-02-19 12:49:24 -0300

    excluindo codigo desnecessário

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let $botao = document.querySelector(".botao");
 
 $botao.addEventListener('click', () => {
 
-    $container.classList.toggle('flipper');
+   
     $container.classList.toggle('sidenav-active');
    
 });
